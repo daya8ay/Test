33b9f23da67bef187935c297089b6bff32fdef55
dayanna <dayanna@dayannas-mbp.dhcp.asu.edu>
2024-09-24 15:44:39 -0700

    helooo

---
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/HelloWorldJavaFXMavenHW3.java
@@ -0,0 +1,38 @@
+package asuHelloWorldJavaFX;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class HelloWorldJavaFXMavenHW3 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+    	System.out.println("ASU Hello World!");
+    	System.out.println("It started!");
+    	
+    	for(int i=0; i<3;i++) {
+    		System.out.print("Dayanna says: Hello World!\n");
+    	}
+        
+        primaryStage.setTitle("Dayanna Bay Hello World Fall 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'Dayanna says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Dayanna says: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
